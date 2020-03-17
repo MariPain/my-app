b8aa9503fad57bd3172363cf134c3327966f3abc
MariPain <57314812+MariPain@users.noreply.github.com>
2020-03-17 16:43:27 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      texto: 'Te vendo la moto',
+    }
+    // this.actualizar = this.actualizar.bind(this)
+  }
+  // actualizar( evento ) {
+  actualizar = (evento) => {
+    console.log(evento.target.value)
+    this.setState({texto: evento.target.value})
+  }
+  render(){
+    const {texto} = this.state;
+    return (
+      <>
+        <Titulo txt={texto}/>
+        <Input cambiaElEstado={this.actualizar}/>
+        <Lista />
+      </>
+    )
+  }
 }
+class Lista extends React.Component {
+  constructor() {
+    super();
+      this.state = {
+        coleccion: [
+          {
+            image: 'neo.jpg',
+            title: 'Moto Neo',
+            text: 'La pequeña gigante. No necesita permiso de conducción',
+            value: '35€'
+          },
+          {
+            image: 'otra.jpg',
+            title: 'Moto dos',
+            text: 'La pequeña gigante. No necesita permiso de conducción',
+            value: '40€'
+          },
+          {
+            image: 'tres.jpg',
+            title: 'Moto gp',
+            text: 'La pequeña gigante. No necesita permiso de conducción',
+            value: '300€'
+          }
+        ]
+      }
+  }
+  render() {
+    const {coleccion} = this.state;
+    return (
+      <ul>
+      {coleccion.map((moto, index) => {
+        return <li key={index}>{moto.image}, {moto.title}, {moto.text}, {moto.value}</li>
+      })}
+      </ul>
+    )
+  }
+}
+class Input extends React.Component {
+  render() {
+    const {cambiaElEstado} = this.props;
+    return (
+      <input type="number" onChange={cambiaElEstado}/>
+    )
+  }
+}
+class Titulo extends React.Component {
+  render() {
+    const {number} = this.props
+    return (
 
+      <p>{number}</p>
+    )
+  }
+}
 export default App;
